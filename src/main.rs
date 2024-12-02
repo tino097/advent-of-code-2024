0678d74f7c01b39cbcb6b083f50ba252398f6aca
tino097 <konstantin.sivakov@gmail.com>
2024-12-02 15:06:46 +0100

    Day 2, part 1

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ mod solutions;
 
 
 fn main() {
-    // Example usage
-    let input = match utils::get_input(1) {
+    let input = match utils::get_input(2) {
         Ok(input_str) => input_str,
         Err(e) => {
             eprintln!("Error: {}", e);
@@ -12,16 +11,24 @@ fn main() {
         }
     };
     
-    match solutions::day1_1::solve_part1(&input){
-        Ok(result) => println!("Distance: {}", result),
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        }
-    };
+    // match solutions::day1_1::solve_part1(&input){
+    //     Ok(result) => println!("Distance: {}", result),
+    //     Err(e) => {
+    //         eprintln!("Error: {}", e);
+    //         std::process::exit(1);
+    //     }
+    // };
 
-    match solutions::day1_2::solve_part2(&input){
-        Ok(result) => println!("Similarity: {}", result),
+    // match solutions::day1_2::solve_part2(&input){
+    //     Ok(result) => println!("Similarity: {}", result),
+    //     Err(e) => {
+    //         eprintln!("Error: {}", e);
+    //         std::process::exit(1);
+    //     }
+    // };
+
+    match solutions::day2_1::solve_part1(&input){
+        Ok(result) => println!("Safe Report: {}", result),
         Err(e) => {
             eprintln!("Error: {}", e);
             std::process::exit(1);
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,2 +1,3 @@
 pub mod day1_1;
 pub mod day1_2;
+pub mod day2_1;
